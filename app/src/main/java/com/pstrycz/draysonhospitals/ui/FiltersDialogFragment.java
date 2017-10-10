4203d917e6016c20de48c57b6fc186e8aca66263
Paweł <stryczniewicz@gmail.com>
2017-10-10 23:22:24 +0200

    spinners added

--- app/src/main/java/com/pstrycz/draysonhospitals/ui/FiltersDialogFragment.java
+++ app/src/main/java/com/pstrycz/draysonhospitals/ui/FiltersDialogFragment.java
@@ -7,10 +7,26 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.pstrycz.draysonhospitals.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FiltersDialogFragment extends DialogFragment {
+
+    @BindView(R.id.subtype)
+    Spinner subtype;
+    @BindView(R.id.sector)
+    Spinner sector;
+    @BindView(R.id.pims)
+    Spinner pims;
 
     @NonNull
     @Override
@@ -18,16 +34,30 @@ public class FiltersDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.dialog_filters, null))
-                .setPositiveButton("OK", (dialog, id) -> {
-                    //TODO
-                });
+        View view = inflater.inflate(R.layout.dialog_filters, null);
+        ButterKnife.bind(this, view);
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+        subtype.setAdapter(adapter);
+
+        builder.setView(view).setPositiveButton("OK", (dialog, id) -> {
+            //TODO
+        });
         return builder.create();
     }
 
     @Override
-        public void onAttach(Context context) {
-            super.onAttach(context);
-        }
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
 
 }
